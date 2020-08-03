086c1493872a84e3f551dbebb8b72b6cfe2a91e8
herculesra <hercules.anselmo@ccc.ufcg.edu.br>
2020-08-03 18:01:18 -0300

    Clear

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Home from './components/Home';
+import Home from './pages/Home';
 
 export default function Routes() {
   return (
