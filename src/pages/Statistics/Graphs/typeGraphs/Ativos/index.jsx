cdc4d79ccdcaa5d57837a4f53618bb2c7b76154e
matheusaraujo <matheus.araujo@ccc.ufcg.edu.br>
2020-08-31 09:45:56 -0300

    add custom tooltip

--- src/pages/Statistics/Graphs/typeGraphs/Ativos/index.jsx
+++ src/pages/Statistics/Graphs/typeGraphs/Ativos/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { ComposedChart, Line, XAxis, YAxis, CartesianGrid, Tooltip,
     Legend, Scatter, ScatterChart, Cell } from 'recharts';
@@ -6,16 +7,43 @@ import { ComposedChart, Line, XAxis, YAxis, CartesianGrid, Tooltip,
 import './style.css'
 
 const Ativos = (props) => {
-
+    const history = useHistory();
     const handleColor = (node) => {
-        if (node.y <= props.data.medidas[node.x - 1].y ){
+        if (node.y < props.data.medidas[node.x - 1].y ){
             return "red"
-        } else if (node.y > props.data.medidas[node.x - 1].y && node.y <= props.data.medidas[node.x - 1].ideal) {
-            return "blue"
-        } else if (node .y > props.data.medidas[node.x - 1].ideal){
+        } else if (node.y >= props.data.medidas[node.x - 1].y && node.y < props.data.medidas[node.x - 1].ideal) {
             return "green"
+        } else if (node .y >= props.data.medidas[node.x - 1].ideal){
+            return "blue"
         }
     }
+
+    const status = (node) => {
+        if (node.y < props.data.medidas[node.x - 1].y ){
+            return "O aluno apresenta dificuldades"
+        } else if (node.y >= props.data.medidas[node.x - 1].y && node.y < props.data.medidas[node.x - 1].ideal) {
+            return "O aluno apresenta desempenho satisfatório"
+        } else if (node .y >= props.data.medidas[node.x - 1].ideal){
+            return "O aluno está acima do esperado"
+        }
+    }
+    const CustomTooltip = ({ active, payload, label }) => {
+        console.log(payload)
+        console.log(label)
+        if (active) {
+          const statusNode = status(payload[0].payload)
+          return (
+            <div className="custom-tooltip">
+              <p className="title-tooltip">{payload[0].payload.id}</p>
+              <p>Períodos ativos: {payload[0].payload.x}</p>
+              <p>Curso Concluído: {payload[0].payload.y}%</p>
+              <p className="desc">{statusNode}</p>
+            </div>
+          );
+        }
+      
+        return null;
+    };
     return (
         <div className={'mainGraphs'}>
             <ScatterChart
@@ -26,14 +54,14 @@ const Ativos = (props) => {
                 }}
             >
                 <CartesianGrid strokeDasharray="3 3"  />
-                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                <Tooltip cursor={{ strokeDasharray: '3 3' }} content={<CustomTooltip />}/>
                 <Legend />
 
                 <XAxis dataKey="x" type="number" name='periodosAtivos' />
                 <YAxis dataKey="y" type="number" name='cursoConcluido' unit='%' />
                 <Scatter name="aluno" data={props.data.alunos} fill="black">
                     {
-                        props.data.alunos.map((entry, index) => <Cell key={`cell-${index}`} fill={handleColor(entry)} />)
+                        props.data.alunos.map((entry, index) => <Cell className="scatter-cell" key={`cell-${index}`} fill={handleColor(entry)} onClick={() => history.push("statistics/ativos/" + entry.id)}/>)
                     }
                 </Scatter>
                     
