de922fe53587fe5068ef97e69477949a69973cc9
herculesra <hercules.anselmo@ccc.ufcg.edu.br>
2020-11-16 15:39:17 -0300

    Buscando dados para sumario da API

--- src/pages/Statistics/Disciplinas/Summary/index.jsx
+++ src/pages/Statistics/Disciplinas/Summary/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import Header from '../../../../components/General/Header';
 import NavBar from '../../../../components/StatisticsComponents/NavBar';
@@ -12,36 +12,56 @@ import { dataSummary, labelSlider } from './utilSummary';
 
 import SliderSummary from './SliderSummary';
 import GraphSummary from './GraphSummary';
-import { useEffect } from 'react';
+
+import api from '../../../../services/api';
 
 const Summary = () => {
 
-    const [dataMaster, setDataMaster] = useState(dataSummary);
+    // const [dataMaster, setDataMaster] = useState(dataSummary);
     const [dataGraph, setDataGraph] = useState([]);
     const [label, setLabel] = useState(labelSlider)
-    const [period, setPeriod] = useState('2010.1');
 
-    const handleSlider = (index) => {
-        const periodo = label[index];      
-        setPeriod(periodo);
-        handlerDataGraph(periodo);
+    const [load, setLoad] = useState(false);
+
+    const [min, setMin] = useState(0);
+    const [max, setMax] = useState(0);
+
+    const handleSlider = (min, max) => {      
+        setMin(min);
+        setMax(max);
+        fetchDataApiWithLabel(min, max);
     }
 
-    const handlerDataGraph = (periodo) => {
-        let index = 0;
-        
-        dataMaster.map( (e,i) => {
-            if(e.periodo == periodo){
-                index = i;
-                return;
-            }
+    const fetchDataApiWithLabel = async (min, max) => {
+        const query = `disciplinas/sumario?de=${label[min]}&ate=${label[max]}`;
+
+        const resSummary = await api.get(`api/estatisticas/${query}`, {});
+
+        if (resSummary.statusText === 'OK') {
+            formatData(resSummary.data);
+        } else {
+            console.log("Error Data Ativos");
+        }
+    }
+
+    const formatData = (data) => {
+
+        setLoad(true);
+
+        const dados = data.map(e => {
+            const  group = e.group;
+            const { lim_inf, lim_sup, q1, q2, q3 } = e.data;
+
+            return { 'label': group, 'y': [lim_inf, q1, q3, lim_sup, q2] };
         });
 
-        setDataGraph(dataMaster[index].data);
+        setDataGraph(dados);
+
+        setLoad(false);
     }
 
-    useEffect(()=>{
-        handlerDataGraph('2010.1');
+    useEffect(() => { 
+        fetchDataApiWithLabel(0, 39);
     },[])
 
     return (
@@ -55,8 +75,10 @@ const Summary = () => {
                         <div className={'listStatistics'}>
                             <SideBar selectedOption={"Sumário"} navSelected={"disciplinas"} listOption={subjectsOptions} names={ nameSubjects }/>
                             <div className={'compStatistics'}>
-                                <SliderSummary changeSlider={handleSlider} labels={labelSlider} period={period}></SliderSummary>
+                                <SliderSummary changeSlider={handleSlider} labels={label} min={min} max={max}></SliderSummary>
+                                {/* {!load ? */}
                                 <GraphSummary data={dataGraph}></GraphSummary>
+                                {/* : null} */}
                                 <br />
                                 <br />
                             </div>
