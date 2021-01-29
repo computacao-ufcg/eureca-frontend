cd797dd89f624e676dcd3de9128b305e9ca865e6
unknown <ekarani.silvestre@ccc.ufcg.edu.br>
2021-01-29 15:14:03 -0300

    grafico de retidos com dados mockados

--- src/pages/Statistics/Students/RetainedStudents/index.jsx
+++ src/pages/Statistics/Students/RetainedStudents/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import Header from '../../../../components/General/Header';
 import NavBar from '../../../../components/StatisticsComponents/NavBar';
@@ -8,9 +8,46 @@ import { nameStudents, navOptions, studentsOptions } from '../../statisticsUtil'
 
 import Title from '../../../../components/General/Title';
 
+import RetainedGraph from './RetainedGraph';
+import RetainedSlider from './RetainedSlider';
+
 import '../../styles.css';
 
+import {teste} from './retainedUtil'
+
+
 const RetainedStudents = () => {
+
+    const [dataRetidos, setDataRetidos] = useState(teste);
+    const [min, setMin] = useState('');
+    const [max, setMax] = useState('');
+
+    useEffect(() => {
+        updateGraph('1981.1', '2019.2')
+    },[]);
+
+
+    const handleSlider = (min, max) => {      
+        setMin(min);
+        setMax(max);
+        
+        updateGraph(min,max);
+    }
+
+    
+    const updateGraph = (min, max) => {
+        let novo = [];
+        
+        teste.map(e => {
+            if(e.periodo >= min && e.periodo <= max) {
+                novo.push(e);
+            }
+        })
+
+        setDataRetidos(novo);
+    }
+    
+    
 
     return(
         <React.Fragment>
@@ -22,6 +59,12 @@ const RetainedStudents = () => {
                     <div className={'modelStatistics'}>
                         <div className={'listStatistics'}>
                             <SideBar selectedOption={"Retidos"} navSelected={"students"} listOption={ studentsOptions } names={ nameStudents }/>
+                            <div className={'compStatistics'}>
+                                <div onMouseUp={() => updateGraph(min, max)}>
+                                    <RetainedSlider changeSlider={handleSlider}/>
+                                </div>
+                                <RetainedGraph data={dataRetidos}/>
+                            </div>
                         </div>
                     </div>
                 </div>
