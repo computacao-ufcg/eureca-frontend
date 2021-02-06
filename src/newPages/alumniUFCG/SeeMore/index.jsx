bf601b033543d9cfb93339b012d5cbfc04ddb3f5
ielepassos <iele.passos@ccc.ufcg.edu.br>
2021-02-05 23:08:02 -0300

    alumni integration

--- src/newPages/alumniUFCG/SeeMore/index.jsx
+++ src/newPages/alumniUFCG/SeeMore/index.jsx
@@ -8,8 +8,22 @@ const SeeMore = () => {
 
     return (
         <React.Fragment>
-            <Header />
-            <h1>SeeMore</h1>
+            <div className ={'main-seemore'}>
+                <div className={'header-container'}>
+                    <Header />
+                </div>
+                <div className={'main-content'}>
+                    <div className = {'main-container-seemore'}>
+    
+                        <div className={'container-title-seemore'}>
+                            <h1>VER MAIS</h1>
+                        </div>
+                    </div>
+                    
+                    
+                </div>
+                
+            </div>
         </React.Fragment>
     );
 }
