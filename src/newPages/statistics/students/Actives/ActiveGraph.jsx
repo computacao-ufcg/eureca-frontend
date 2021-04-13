6a59a46674fdd6e240ce6e5f85aa652ae6b0e512
herculesra <hercules.anselmo@ccc.ufcg.edu.br>
2021-04-13 17:00:17 -0300

    New Alterations to Actives

--- src/newPages/statistics/students/Actives/ActiveGraph.jsx
+++ src/newPages/statistics/students/Actives/ActiveGraph.jsx
@@ -1,91 +1,103 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
-import { Loader } from 'rsuite';
+import { XAxis, YAxis, CartesianGrid, Tooltip, AreaChart, Area } from 'recharts';
 
-import { XAxis, YAxis, CartesianGrid, Tooltip, Scatter, ScatterChart } from 'recharts';
-
-import { getDataScatter } from './activeUtil';
+import { Switch } from 'pretty-checkbox-react'
+import '@djthoms/pretty-checkbox';
 
 import './activeGraph.css';
 
+import { optionsActives } from './activeUtil';
+
 const ActiveGraph = (props) => {
-    const [loadding, setLoadding] = useState(true);
 
-    // for graph
-    const [normal, setNormal] = useState([]);
-    const [late, setLate] = useState([]);
-    const [advanced, setAdvanced] = useState([]);
-    const [critical, setCritical] = useState([]);
-    const [notApplicable, setNotApplicable] = useState([]);
-    const [unfeasible, setUnfeasible] = useState([]);
+    const [tag0, setTag0] = useState("riskClassCount.inaccurate");
+    const [tag1, setTag1] = useState("riskClassCount.safe");
+    const [tag2, setTag2] = useState("riskClassCount.low");
+    const [tag3, setTag3] = useState("riskClassCount.average");
+    const [tag4, setTag4] = useState("riskClassCount.high");
+    const [tag5, setTag5] = useState("riskClassCount.unfeasible");
+    const [tag6, setTag6] = useState("riskClassCount.notApplicable");
 
-    useEffect(() => {
-        const loaddingData = () => {
-            setLoadding(true);
+    const tag0color = "#FFAE03";
+    const tag1color = "#9BC53D";
+    const tag2color = "#932551";
+    const tag3color = "#4DA852";
+    const tag4color = "#4FC6E3";
+    const tag5color = "#E71D36";
+    const tag6color = "#001021";
 
-            const [normal, late, advanced, critical, notApplicable, unfeasible] = getDataScatter(props.data);
-
-            setNormal(normal);
-            setLate(late);
-            setAdvanced(advanced);
-            setCritical(critical);
-            setNotApplicable(notApplicable);
-            setUnfeasible(unfeasible);
-
-            setLoadding(false);
+    const handleCheck = (event) => {
+        switch (event.target.name) {
+            case 'tag0':
+                event.target.checked ? setTag0("riskClassCount.inaccurate") : setTag0('');
+                break
+            case 'tag1':
+                event.target.checked ? setTag1("riskClassCount.safe") : setTag1('');
+                break
+            case 'tag2':
+                event.target.checked ? setTag2("riskClassCount.low") : setTag2('');
+                break
+            case 'tag3':
+                event.target.checked ? setTag3("riskClassCount.average") : setTag3('');
+                break
+            case 'tag4':
+                event.target.checked ? setTag4("riskClassCount.high") : setTag4('');
+                break
+            case 'tag5':
+                event.target.checked ? setTag5("riskClassCount.unfeasible") : setTag5('');
+                break
+            case 'tag6':
+                event.target.checked ? setTag6("riskClassCount.notApplicable") : setTag6('');
+                break
+            default:
+                console.error('error selection tags');
         }
-
-        loaddingData();
-    }, [props.data]);
-
-    const CustomTooltip = ({ active, payload }) => {
-        if (active) {
-            return (
-                <div className="custom-tooltip">
-                    <p>Períodos Integralizados: {payload[0].payload.completedTerms}</p>
-                    <p>Créditos Integralizados: {(payload[0].payload.progress * 100).toFixed(2)}%</p>
-                </div>
-            );
-        }
-
-        return null;
-    };
+    }
 
     return (
         <div className="actives-graph-container">
-            {loadding ? <Loader content="Carregando..." vertical></Loader> :
-                <React.Fragment>
-                    <div className="graph-main">
-                        <ScatterChart
-                            width={1000}
-                            height={700}
-                            margin={{
-                                top: 20, right: 80, bottom: 20, left: 20,
-                            }}
-                        >
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <Tooltip cursor={{ strokeDasharray: '3 3' }} content={CustomTooltip} />
-                            <XAxis
-                                dataKey="completedTerms"
-                                type="number"
-                            />
-                            <YAxis
-                                dataKey="progress"
-                                type="number"
-                            />
-                            <Scatter data={normal} fillOpacity={0.5} fill={"blue"} name={"normal"}></Scatter>
-                            <Scatter data={late} fillOpacity={0.5} fill={"green"} name={"late"}></Scatter>
-                            <Scatter data={advanced} fillOpacity={0.5} fill={"purple"} name={"advanced"}></Scatter>
-                            <Scatter data={critical} fillOpacity={0.5} fill={"red"} name={"critical"}></Scatter>
-                            <Scatter data={notApplicable} fillOpacity={0.5} fill={"yellow"} name={"notApplicable"}></Scatter>
-                            <Scatter data={unfeasible} fillOpacity={0.5} fill={"black"} name={"unfeasible"}></Scatter>
-                        </ScatterChart>
+            <div className="graph-main">
+                <AreaChart
+                    width={800}
+                    height={500}
+                    data={props.data}
+                    margin={{
+                        top: 10,
+                        right: 30,
+                        left: 0,
+                        bottom: 0
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="admissionTerm" />
+                    <YAxis yAxisId="right" />
+                    <Tooltip />
+                    <Area yAxisId="right" dataKey={tag0} name={optionsActives[0]} type="monotone" stackId="1" stroke={tag0color} fill={tag0color} />
+                    <Area yAxisId="right" dataKey={tag1} name={optionsActives[1]} type="monotone" stackId="1" stroke={tag1color} fill={tag1color} />
+                    <Area yAxisId="right" dataKey={tag2} name={optionsActives[2]} type="monotone" stackId="1" stroke={tag2color} fill={tag2color} />
+                    <Area yAxisId="right" dataKey={tag3} name={optionsActives[3]} type="monotone" stackId="1" stroke={tag3color} fill={tag3color} />
+                    <Area yAxisId="right" dataKey={tag4} name={optionsActives[4]} type="monotone" stackId="1" stroke={tag4color} fill={tag4color} />
+                    <Area yAxisId="right" dataKey={tag5} name={optionsActives[5]} type="monotone" stackId="1" stroke={tag5color} fill={tag5color} />
+                    <Area yAxisId="right" dataKey={tag6} name={optionsActives[6]} type="monotone" stackId="1" stroke={tag6color} fill={tag6color} />
+                </AreaChart>
+                <p className="graph-label-y">Ativos</p>
+                <p className="graph-label-x">Período de Admissão</p>
+            </div>
 
-                        <p className="graph-label-y">Créditos Integralizados</p>
-                        <p className="graph-label-x">Períodos Integralizados</p>
-                    </div>
-                </React.Fragment>
-            }
+            <div className="optionsActives">
+                {optionsActives.map((e, index) =>
+                    <label key={"label" + index}>
+                        <Switch
+                            shape="fill"
+                            className={"switch" + index}
+                            key={"check" + index}
+                            name={"tag" + index}
+                            id={index}
+                            defaultChecked
+                            onChange={handleCheck}
+                        />{" " + optionsActives[index]}</label>)}
+            </div>
         </div>
     )
 }
