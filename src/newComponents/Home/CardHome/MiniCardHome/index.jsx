e74397fb2cd030de4335c9cd5c10159b4d430be1
Natália Salvino <natalia.andre@ccc.ufcg.edu.br>
2021-05-14 14:24:12 -0300

    new data to subjectsCardHome

--- src/newComponents/Home/CardHome/MiniCardHome/index.jsx
+++ src/newComponents/Home/CardHome/MiniCardHome/index.jsx
@@ -64,4 +64,16 @@ const MiniCardHome5 = (props) => {
     )
 }
 
-export { MiniCardHome1, MiniCardHome2, MiniCardHome3, MiniCardHome4,MiniCardHome5};
+const MiniCardHomeSubjects = (props) => {
+
+  return (
+      <div className="main-mini-card-home-5">
+          <div className="number-mini-card-home" >
+              <div className="number-mini-card-home-content">{props.number}</div>
+          </div>
+          <div className="legend-mini-card-home-subjects">{props.legend}</div>
+      </div>
+  )
+}
+
+export { MiniCardHome1, MiniCardHome2, MiniCardHome3, MiniCardHome4,MiniCardHome5, MiniCardHomeSubjects};
