616b01c7a98b323db01d58b6c1cf95ba1521069e
iele passos <iele.passos@ccc.ufcg.edu.br>
2021-06-08 16:02:20 -0300

    Alumni Card refactor

--- src/components/Home/CardHome/AlumniCardHome/AlumniCards/index.jsx
+++ src/components/Home/CardHome/AlumniCardHome/AlumniCards/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { MiniCardHome1, MiniCardHome2 } from "../../MiniCardHome";
+import { MiniCardHome1,MiniCardHomeRightBottom,MiniCardHome4,MiniCardHome3,MiniCardHome5,MiniCardHome2} from "../../MiniCardHome";
 import "./style.css";
 
 const AlumniCards = props => {
@@ -17,10 +17,11 @@ const AlumniCards = props => {
               </div>
             </div>
             <div className='alumni-summary-left-cards'>
-              <MiniCardHome2 number={0} legend={"EMP. CONSOLIDADAS"} />
+              <MiniCardHome4
+              number={data.numberMatchedAlumniCourse || 0}
+              legend={"RASTREADOS"} />
               <MiniCardHome1
-                number={data.numberGovernmentEmployedCourse || 0}
-                legend={"NO GOVERNO"}
+              number={0} legend={"EMP. CONSOLIDADAS"}
               />
             </div>
           </div>
@@ -30,16 +31,16 @@ const AlumniCards = props => {
               legend={"NA INDÚSTRIA"}
             />
             <MiniCardHome1
-              number={data.numberAcademyEmployedCourse || 0}
-              legend={"NA ACADEMIA"}
+              number={data.numberGovernmentEmployedCourse || 0}
+                legend={"NO GOVERNO"}
+            />
+            <MiniCardHome1
+             number={data.numberAcademyEmployedCourse || 0}
+             legend={"NA ACADEMIA"}
             />
             <MiniCardHome1
               number={data.numberOngEmployedCourse || 0}
               legend={"EM ONGS"}
-            />
-            <MiniCardHome1
-              number={data.numberMatchedAlumniCourse || 0}
-              legend={"CADASTRADOS"}
             />
           </div>
         </div>
