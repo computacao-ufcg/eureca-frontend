152910d62437e52defe4475e0f8e4b3ff0351090
iele passos <iele.passos@ccc.ufcg.edu.br>
2021-07-19 15:36:26 -0300

    fix: new labels at enrollments graph

--- src/pages/statistics/Enrollments/Graph/index.jsx
+++ src/pages/statistics/Enrollments/Graph/index.jsx
@@ -15,50 +15,35 @@ import "./style.css";
 const EnrollmentsGraph = props => {
   console.log(props);
   return (
-    <BarChart
-      width={680}
-      height={350}
-      data={props.data}
-      margin={{
-        top: 30,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray='3 3' />
-      <XAxis
-        dataKey='discipline'
-        label={{
-          value: "Disciplinas",
-          position: "insideBottomRight",
-          dy: 10,
+    <div className='main-enrollments-graph'>
+      <BarChart
+        width={680}
+        height={350}
+        data={props.data}
+        margin={{
+          top: 30,
+          right: 30,
+          left: 20,
+          bottom: 5,
         }}
-        padding={{
-          bottom: 10,
-          top: 20,
-        }}
-      />
-      <YAxis
-        dataKey={props.variable || "totalEnrollments"}
-        label={{
-          value: props.label,
-          angle: -90,
-          position: "insideBottomLeft",
-        }}
-        padding={{
-          top: 50,
-        }}
-      />
-      <Tooltip />
-      <Legend
-        verticalAlign='top'
-        iconSize={10}
-        iconType='circle'
-        payload={[{ value: props.label, type: "circle", color: "#886859" }]}
-      />
-      <Bar dataKey={props.variable} fill='#886859' key='Número de turmas' />
-    </BarChart>
+      >
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='discipline' />
+        <YAxis dataKey={props.variable || "totalEnrollments"} />
+        <Tooltip />
+        <Legend
+          verticalAlign='top'
+          iconSize={10}
+          iconType='circle'
+          payload={[{ value: props.label, type: "circle", color: "#886859" }]}
+        />
+        <Bar dataKey={props.variable} fill='#886859' key='Número de turmas' />
+      </BarChart>
+      <p className='graph-label-y-enrollments'>
+        {props.label || "Total de Matrículas"}
+      </p>
+      <p className='graph-label-x-enrollments'>Disciplinas</p>
+    </div>
   );
 };
 
