a016ba321e2d38ada9fbd97b8b70521898266cd2
iele passos <iele.passos@ccc.ufcg.edu.br>
2021-08-11 20:13:44 -0300

    refact: refact UAL see more page

---
+++ src/pages/statistics/Teachers/UAL/index.jsx
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router";
+import { SelectPicker } from "rsuite";
+import { FiArrowLeft } from "react-icons/fi";
+
+import TeachersSlider from "../Slider";
+import TeachersGraph from "./Graph";
+
+import Export from "../../../../components/Export";
+import Header from "../../../../components/Header";
+
+import { api_EB } from "../../../../services/api";
+
+import "./style.css";
+
+const UALTeachers = () => {
+  const [variable, setVariable] = useState("successRate");
+  const [label, setLabel] = useState("Taxa de sucesso");
+  const [data, setData] = useState([]);
+  const [dataExport, setDataExport] = useState([]);
+
+  const handleVariableChange = variable => {
+    setVariable(variable);
+    const proposedLabel = variables.find(item => item.value === variable);
+    setLabel(proposedLabel.label);
+  };
+
+  useEffect(() => {
+    (async function () {
+      const query = `/statistics/teachers/summary`;
+      try {
+        const res = await api_EB.get(query, {
+          headers: {
+            "Authentication-Token": sessionStorage.getItem("eureca-token"),
+          },
+        });
+
+        if (res) {
+          setData(res.data);
+          setDataExport(res.dataCSV.teachers);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, []);
+
+  const variables = [
+    {
+      label: "Taxa de sucesso",
+      value: "successRate",
+      role: "Master",
+    },
+    {
+      label: "Taxa de reprovação por nota",
+      value: "averageFailDueToGrade",
+      role: "Master",
+    },
+    {
+      label: "Taxa de reprovação por falta",
+      value: "averageFailDueToAbsences",
+      role: "Master",
+    },
+    {
+      label: "Taxa de trancamento",
+      value: "suspendedRate",
+      role: "Master",
+    },
+    {
+      label: "Número de matrículas",
+      value: "totalEnrollments",
+      role: "Master",
+    },
+  ];
+  const history = useHistory();
+
+  return (
+    <React.Fragment>
+      <Header />
+      <div className='teachers-ual-main'>
+        <div className='teachers-ual-content'>
+          <div className='backdot'>
+            <span onClick={() => history.goBack()}>
+              <FiArrowLeft size={25} />
+            </span>
+          </div>
+          <div className='teachers-ual-slider'>
+            <div className='teachers-ual-title'>Docentes</div>
+            <TeachersSlider changeSlider={() => {}} />
+            <div className='graph'>
+              <TeachersGraph variable={variable} data={data} label={label} />
+              <div className='selectors'>
+                <h6>Variável</h6>
+                <SelectPicker
+                  onChange={value => handleVariableChange(value)}
+                  data={variables}
+                  className='selector-teachers'
+                  defaultValue={variable}
+                  searchable={false}
+                  cleanable={false}
+                />
+              </div>
+            </div>
+            <Export data={dataExport} name={"teachers"} />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default UALTeachers;
--- src/routes.js
+++ src/routes.js
@@ -21,6 +21,7 @@ import EnrollmentsComplementary from "./pages/statistics/Enrollments/Complementa
 import UASCTeachers from "./pages/statistics/Teachers/UASC"
 import UAMatTeachers from "./pages/statistics/Teachers/UAMat";
 import UAEstTeachers from "./pages/statistics/Teachers/UAEst";
+import UALTeachers from "./pages/statistics/Teachers/UAL";
 
 import StudentsGlossary from "./pages/statistics/students/Glossary";
 import TeachersGlossary from "./pages/statistics/Teachers/Glossary";
@@ -30,7 +31,6 @@ import RetentionGlossary from "./pages/statistics/Retention/Glossary";
 import AlumniGlossary from "./pages/alumniUFCG/Glossary";
 
 import Subjects from "./pages/statistics/Subjects";
-
 
 const Routes = () => {
   return (
@@ -43,8 +43,8 @@ const Routes = () => {
         <PrivateRoute exact path='/statistics/students/dropout' component={() => <Dropout />} />
         <PrivateRoute exact path='/statistics/students/glossary' component={() => <StudentsGlossary />} />
         <PrivateRoute exact path='/statistics/teachers/glossary' component={() => <TeachersGlossary />} />
-        <PrivateRoute exact path='/statistics/enrollments/glossary' component={() => <EnrollmentsGlossary />} />
         <PrivateRoute exact path='/statistics/subjects/glossary' component={() => <SubjectsGlossary />} />
+        <PrivateRoute exact path='/statistics/enrollments/glossary' component={() => <EnrollmentsGlossary />} />
         <PrivateRoute exact path='/statistics/retention/glossary' component={() => <RetentionGlossary />} />
         <PrivateRoute exact path='/statistics/retention/students' component={() => <RetentionStudents />} />
         <PrivateRoute exact path='/statistics/retention/subjects' component={() => <RetentionSubjects />} />
@@ -55,6 +55,7 @@ const Routes = () => {
         <PrivateRoute exact path='/statistics/teachersUASC' component={() => <UASCTeachers />} />
         <PrivateRoute exact path='/statistics/teachersUAMat' component={() => <UAMatTeachers/>} />
         <PrivateRoute exact path='/statistics/teachersUAEst' component={() => <UAEstTeachers />} />
+        <PrivateRoute exact path='/statistics/teachersUAL' component={() => <UALTeachers />} />
         <PrivateRoute exact path='/alumniUFCG/glossary' component={() => <AlumniGlossary />} />
         <PrivateRoute exact path='/statistics/subjects' component={() => <Subjects />} />
         <PrivateRoute exact path='/alumniufcg/updatedata' component={() => <UpdateData />} />
