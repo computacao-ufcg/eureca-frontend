d5793a196010cf6cfd6d1df7053c5c84311708bf
ielepassos <iele.passos@ccc.ufcg.edu.br>
2021-08-30 12:26:07 -0300

    fix: fix query from UAEst teachers

--- src/pages/statistics/Teachers/UAEst/index.jsx
+++ src/pages/statistics/Teachers/UAEst/index.jsx
@@ -14,7 +14,7 @@ import { api_EB } from "../../../../services/api";
 import "./style.css";
 import {
   baseTeachersEndpoint,
-  endpointWithCourseAndCurriculum,
+  endpointWithCourseAndCurriculumAlternative,
   eurecaAuthenticationHeader,
 } from "../../../../config/defaultValues";
 
@@ -32,8 +32,8 @@ const UAEstTeachers = () => {
 
   useEffect(() => {
     (async function () {
-      const endpoint = `${baseTeachersEndpoint}/summary`;
-      const query = endpointWithCourseAndCurriculum(endpoint);
+      const endpoint = `${baseTeachersEndpoint}?academicUnitId=${1114}`;
+      const query = endpointWithCourseAndCurriculumAlternative(endpoint);
 
       try {
         const res = await api_EB.get(query, eurecaAuthenticationHeader);
