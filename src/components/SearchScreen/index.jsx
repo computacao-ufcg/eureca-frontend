4eacca03937097e3d0f44a55d77fb0ac533631fb
ielepassos <iele.passos@ccc.ufcg.edu.br>
2021-10-21 01:03:44 -0300

    add checkbox

--- src/components/SearchScreen/index.jsx
+++ src/components/SearchScreen/index.jsx
@@ -6,7 +6,8 @@ const SearchScreen = props => {
   return (
     <React.Fragment>
       <div className='container-title-search'>
-        <h1>Buscar e-mails por {props.title}</h1>
+      <h1>Buscar e-mails por {props.title}</h1>
+        <input type='checkbox'/>
       </div>
       <div className='selects'>{props.children}</div>
     </React.Fragment>
