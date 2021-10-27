824667f5ab41959b6f3b86487a32ab961fe07588
ielepassos <iele.passos@ccc.ufcg.edu.br>
2021-10-27 20:45:07 -0300

    fix(ResultsTable):add data

--- src/pages/communication/table/resultsTable.jsx
+++ src/pages/communication/table/resultsTable.jsx
@@ -4,20 +4,36 @@ import { Table } from "rsuite";
 const { Column, HeaderCell,Cell} = Table;
 
 const ResultsTable = (props) => {
+  console.log(props)
+  const data = props.listData;
+
+  function handleData(data){
+    const keys = Object.keys(data);
+    const result = [];
+
+    keys.forEach(element => {
+      const student = {registration:element,name:data[element].name,email:data[element].email}
+      result.push(student)
+    });
+
+    return result;
+    
+  }
+  const students = handleData(data);
 
   return (
     <div className='table-email'>
       <Table
         height={480}
         width={750}
-        data={props.listData}
+        data={students}
         onRowClick={data => {
           console.log(data);
         }}
       >
         <Column width={250} align='center' fixed>
           <HeaderCell>Matrícula</HeaderCell>
-          <Cell dataKey="id" />
+          <Cell dataKey="registration" />
         </Column>
         <Column width={250} align='center' fixed>
           <HeaderCell>Nome</HeaderCell>
