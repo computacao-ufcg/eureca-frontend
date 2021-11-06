ee7efc0d9664284ad341233f6a7e7b8e5af1f5b8
ielepassos <iele.passos@ccc.ufcg.edu.br>
2021-11-05 21:37:26 -0300

    add initial design

--- src/pages/services/index.jsx
+++ src/pages/services/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import Header from "../../components/Header";
+import { SelectPicker } from "rsuite";
 import "./style.css";
 
 const ServicesPage = () => {
@@ -9,12 +10,56 @@ const ServicesPage = () => {
   return (
     <React.Fragment>
       <Header />
-      <div className='alumni-main'>
-        <div className='alumni-content'>
+      <div className='services-main'>
+        <div className='services-content'>
           <div className='backdot'>
             <span onClick={() => history.goBack()}>
               <FiArrowLeft size={25} />
             </span>
+          </div>
+          <div className='all-selects'>
+            <div className='service-title'>
+              <h1>Pré-matrícula Individual</h1>
+            </div>
+            <div className='selects-individual-enrollment'>
+              <div>
+                <p>Período</p>
+                <input id='ipt-name' type='text' placeholder='' />
+              </div>
+              <div>
+                <p>Matrícula</p>
+                <SelectPicker data={0} searchable={true} cleanable={false} style={{ width: 224 }} />
+              </div>
+              <div>
+                <p>Número de Créditos</p>
+                <SelectPicker data={0} searchable={false} cleanable={false} style={{ width: 120 }} />
+              </div>
+              <div>
+                <p>Prioridade em Disciplinas Obrigatórias</p>
+                <SelectPicker data={0} searchable={false} cleanable={false} style={{ width: 150 }} />
+              </div>
+              <div>
+                <p>Prioridade em Disciplinas Optativas</p>
+                <SelectPicker data={0} searchable={false} cleanable={false} style={{ width: 150 }} />
+              </div>
+              <div>
+                <p>Prioridade em Disciplinas Eletivas</p>
+                <SelectPicker data={0} searchable={false} cleanable={false} style={{ width: 150 }} />
+              </div>
+            </div>
+            <div className='service-title'>
+              <h1>Pré-matrícula em Lotes</h1>
+            </div>
+            <div className='selects-lot-enrollment'>
+              <div>
+                <p>Períodos</p>
+                <SelectPicker data={0} searchable={false} cleanable={false} style={{ width: 150 }} />
+              </div>
+              <div>
+              <p>Configuração</p>
+                <input type='file'/>
+              </div>
+            </div>
           </div>
         </div>
       </div>
